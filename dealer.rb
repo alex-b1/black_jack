ecf14751cb5b10353587e8d28055e082191b8228
Alex Bogomolov <a.bogomolov@balance-pl.ru>
2021-04-23 00:11:13 +0000

    add new methods

--- dealer.rb
+++ dealer.rb
@@ -4,5 +4,9 @@ require_relative './bank.js'
 require_relative './player.js'
 
 class Dealer < Player
+  TYPE = :dealer
 
+  def type
+    TYPE
+  end
 end
--- game.rb
+++ game.rb
@@ -10,6 +10,14 @@ class Game
   include Helper
   attr_reader :user, :dealer, :deck, :bank, :balance
 
+  def initialize
+    @command_list = {
+        1 => { title: 'Пропустить', command: -> { pass } },
+        2 => { title: 'Добавить карту', command: -> { |i| add_card i } },
+        3 => { title: 'Открыть карты', command: -> { open_cards } },
+    }
+  end
+
   def init
     print 'Ведите ваше имя: '
     name = string
@@ -20,10 +28,51 @@ class Game
     @bank = Bank.new
     @balance = bank.balance
     start
+
   end
 
   def start
     issue_cards
+    place_bet
+
+    user_actions
+
+    loop do
+      break if check_cards_length
+      user_actions
+      dealer_actions
+    end
+
+  end
+
+  def check_cards_length
+    user.cards.length  == 3 && user.cards.length  == 3
+  end
+
+  def user_actions
+    show_tasks
+    task_number = task
+    break if task_number == 1
+    puts @command_list[task_number][:command].call(user)
+  end
+
+  def dealer_actions
+    if validate_cards_count dealer
+      break if pass
+    else
+      add_card dealer
+    end
+  end
+
+  def show_tasks
+    puts 'Введите действие : '
+    @command_list.each do |k, v|
+      puts "#{k} - #{v[:title]}"
+    end
+  end
+
+  def task
+    gets.chomp.to_i
   end
 
   def issue_cards
@@ -40,7 +89,36 @@ class Game
     bank.balance.put_in(20)
   end
 
-  def get_cards
+  def pass
+    true
+  end
 
+  def add_card(player)
+    player.cards << @deck.slice!(0, 1) if validate_cards_length(player)
+    player.calculate_count
+  end
+
+  def open_cards
+    user_count = user.count
+    dealer_count = dealer.count
+    puts "Сумма очков у вам: #{user_count}"
+    puts "Сумма очков у дилера: #{dealer_count}"
+  end
+
+  def calculate_result
+
+  end
+
+  def validate_cards_length(player)
+    raise 'Должно быть 2 карты на руках' if player.cards.length != 2
+  end
+
+  def validate_cards_count(player)
+    raise 'Должно быть 17 или более очков' if player.count > 17
+  end
+
+  def continue
+    puts 'нажмите любую клавишу чтобы продолжить'
+    gets
   end
 end
